b4c849988d146be416e4ad98e3aa6708b1859468
gelugu <kraev.mixail@gmail.com>
2024-06-22 07:25:17 +0400

    parse debug arg

--- arguments.go
+++ arguments.go
@@ -13,6 +13,7 @@ func parseArgs() Arguments {
 	args := Arguments{}
 
 	flag.StringVar(&args.ConfigPath, "config", "/etc/newsletter/config.yaml", "Path to the configuration file")
+	flag.BoolVar(&args.Debug, "debug", false, "Enable debug mode")
 
 	flag.Parse()
 
